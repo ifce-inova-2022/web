28c1d0cf98466e13ce8ba9ba4cdb41745e163d75
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-10-27 11:43:37 -0300

    refactor

--- src/components/FieldInput.tsx
+++ src/components/FieldInput.tsx
@@ -1,4 +1,4 @@
-import { Eye, EyeClosed } from "phosphor-react";
+import { Eye, EyeClosed, Pencil } from "phosphor-react";
 import { Dispatch, SetStateAction, useState } from "react";
 
 interface FieldProps {
@@ -8,10 +8,11 @@ interface FieldProps {
   label: string;
   icon?: React.ReactNode;
   placeholder: string;
+  isEditable?: boolean;
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
   required?: boolean;
-  width: "50%" | "100%";
+  width?: "50%" | "100%";
 }
 
 export function FieldInput({
@@ -23,11 +24,13 @@ export function FieldInput({
   initialType = type,
   value,
   setValue,
+  isEditable = false,
   required = true,
-  width,
+  width = "100%",
 }: FieldProps) {
   const [isVisible, setIsVisible] = useState(false);
   const [typed, setTyped] = useState(type);
+  const [toggleEdit, setToggleEdit] = useState(isEditable);
 
   function handleChange(e: { target: { value: string } }) {
     setValue(e.target.value);
@@ -41,13 +44,10 @@ export function FieldInput({
 
   const isPassword = initialType === "password";
 
-  const widthInput =
-  width === "50%"
-      ? "w-[50%] flex flex-col gap-1 group"
-      : "w-full flex flex-col gap-1 group";
+  const widthInput = width === "50%" ? "w-[50%] " : "w-full";
 
   return (
-    <fieldset className={`${widthInput}`}>
+    <fieldset className={`${widthInput} flex flex-col gap-1 group`}>
       <label className="text-zinc-800 font-semibold" htmlFor={id}>
         {label}
       </label>
@@ -61,10 +61,13 @@ export function FieldInput({
         <input
           value={value}
           onChange={handleChange}
-          className="w-full min-w-min p-2 pl-10 h-12
+          disabled={toggleEdit}
+          className={`${
+            toggleEdit ? "disabled:cursor-not-allowed disabled:border-none" : ""
+          } w-full min-w-min p-3 ${icon ? "pl-10" : ""}
           bg-zinc-200 border-b-2 border-r-2 border-zinc-300 
           outline-none hover:border-zinc-500 focus:border-zinc-500
-          rounded-md transition-colors"
+          rounded-md transition-colors`}
           id={id}
           type={typed}
           placeholder={placeholder}
@@ -73,10 +76,24 @@ export function FieldInput({
         {isPassword ? (
           <button
             type={"button"}
-            className="absolute top-2.5 right-3 text-xl"
+            className="absolute top-3 right-3 text-xl"
             onClick={handleClick}
           >
             {isVisible ? <Eye /> : <EyeClosed />}
+          </button>
+        ) : (
+          ""
+        )}
+        {isEditable ? (
+          <button
+            className={`${
+              toggleEdit
+                ? "bg-zinc-300"
+                : "bg-custom-green-default text-zinc-50"
+            } absolute top-0 right-0 text-xl hover:scale-[1.02] p-3 rounded transition-colors`}
+            onClick={() => setToggleEdit(!toggleEdit)}
+          >
+            <Pencil size={24} />
           </button>
         ) : (
           ""
