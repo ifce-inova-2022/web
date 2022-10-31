d5fddc932cdf7ff6382568cf0c66ac06853357a6
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-10-31 16:06:12 -0300

    feat(chart): implements chart component

--- src/components/consumption/Diary.tsx
+++ src/components/consumption/Diary.tsx
@@ -1,4 +1,5 @@
 import { Button } from "../Button";
+import { ChartDiary } from "../charts/ChartDiary";
 import { SectionDashboard } from "../SectionDashboard";
 
 export function Diary() {
@@ -12,9 +13,10 @@ export function Diary() {
         <span className="text-xl font-medium">Período</span>
         <span>14 de outubro de 2022</span>
       </div>
-      <div className="mt-8">
+      <div className="mt-8 flex flex-col">
         <span className="text-2xl font-bold">Gráfico de consumo diário</span>
-        <img className="h-72 mt-4" src="/chart-demanda-ativa.svg" alt="" />
+        <ChartDiary />
+        {/* <img className="h-72 mt-4" src="/chart-demanda-ativa.svg" alt="" /> */}
       </div>
       <div className="flex flex-row justify-between">
         <Button typeColor="primary" sizeWidth="50%" title="Retornar período" />
