da35fc7b7d7f4dedcee5d93e9d1dfa4439831989
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-11-06 18:53:24 -0300

    fix(chart): corrigindo bugs

--- src/components/charts/ChartDiary.tsx
+++ src/components/charts/ChartDiary.tsx
@@ -7,7 +7,10 @@ import {
   Title,
   Tooltip,
   Legend,
+  ElementChartOptions,
+  ChartOptions,
 } from "chart.js";
+import annotationPlugin from "chartjs-plugin-annotation";
 import { Line } from "react-chartjs-2";
 
 ChartJS.register(
@@ -17,10 +20,11 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  annotationPlugin
 );
 
-export const options = {
+const options = {
   responsive: true,
   interaction: {
     mode: "index" as const,
@@ -28,20 +32,39 @@ export const options = {
   },
   stacked: false,
   plugins: {
+    annotation: {
+      annotations: {
+        box1: {
+          type: "box" as const,
+          display: true,
+          xMin: 18,
+          xMax: 22,
+          yMin: 0,
+          yMax: 30,
+          backgroundColor: "rgba(248, 161, 98, 0.60)",
+        },
+      },
+    },
     title: {
       display: true,
-    //   text: "Chart.js Line Chart - Multi Axis",
+      //   text: "Chart.js Line Chart - Multi Axis",
     },
   },
   scales: {
     y: {
       type: "linear" as const,
-      display: true,
+      title: {
+        display: true,
+        text: "Consumo Diário (kW)",
+      },
       position: "left" as const,
     },
     y1: {
       type: "linear" as const,
-      display: true,
+      title: {
+        display: true,
+        text: "Consumo Diário (R$)",
+      },
       position: "right" as const,
       grid: {
         drawOnChartArea: false,
@@ -50,30 +73,65 @@ export const options = {
   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+const labels = [
+  "",
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+];
 
 export const data = {
   labels,
   datasets: [
     {
-      label: "Dataset 1",
-      data: [0, 18, 14, 3, 14, 25, 30],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      label: "Demanda Ativa",
+      data: [
+        0, 18, 14, 3, 14, 18, 14, 3, 14, 18, 14, 3, 14, 18, 14, 3, 14, 18, 14,
+        3, 14, 18, 13, 12, 16,
+      ],
+      borderColor: "rgb(24, 255, 0)",
+      backgroundColor: "rgb(24, 255, 0)",
       yAxisID: "y",
     },
     {
-      label: "Dataset 2",
-      data: [0, 10, 5, 2, 20, 30, 45],
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
+      label: "Demanda Reativa",
+      data: [
+        0, 10, 5, 2, 20, 30, 45, 0, 10, 5, 2, 20, 30, 45, 0, 10, 5, 2, 20, 30,
+        45, 0, 10, 5, 15,
+      ],
+      borderColor: "rgb(255, 0, 0)",
+      backgroundColor: "rgb(255, 0, 0)",
       yAxisID: "y1",
     },
     {
-      label: "Dataset 3",
-      data: [3, 13, 16, 5, 12, 23, 19],
-      borderColor: "rgb(235, 53, 114)",
-      backgroundColor: "rgba(235, 53, 114, 0.5)",
+      label: "Consumo em Reais (R$)",
+      data: [
+        3, 13, 16, 5, 12, 23, 19, 3, 13, 16, 5, 12, 23, 19, 3, 13, 16, 5, 12,
+        23, 19, 14, 13, 19, 26,
+      ],
+      borderColor: "rgb(15, 38, 241)",
+      backgroundColor: "rgb(15, 38, 241)",
       yAxisID: "y1",
     },
   ],
