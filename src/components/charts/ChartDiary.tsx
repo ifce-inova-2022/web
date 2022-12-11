52dd11a623e58b8acc35ee83936fb1a58d80a726
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-12-11 20:40:43 -0300

    fix(modal): pegando dados do objeto

--- src/components/charts/ChartDiary.tsx
+++ src/components/charts/ChartDiary.tsx
@@ -17,7 +17,12 @@ import {
   getElementsAtEvent,
 } from "react-chartjs-2";
 import annotationPlugin from "chartjs-plugin-annotation";
-import { dataIntervalMean, dataIntervalMeanConsumiption, dataIntervalMeanDA, dataIntervalMeanDR } from "../../utils/dataExemple";
+import {
+  dataIntervalMean,
+  dataIntervalMeanConsumiption,
+  dataIntervalMeanDA,
+  dataIntervalMeanDR,
+} from "../../utils/dataExemple";
 import { Modal } from "../Modal";
 
 ChartJS.register(
@@ -74,7 +79,7 @@ export const options = {
             content: "Em ponta",
             color: "black",
             font: {
-              size: 16
+              size: 16,
             },
           },
           display: true,
@@ -88,37 +93,34 @@ export const options = {
     },
     legend: {
       position: "bottom" as const,
+      usePointStyle: true,
     },
   },
 };
 
 const labels = [
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19,
-  20,
-  21,
-  22,
-  23,
-  24,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 24,
 ];
+
+// soma
+// function accumulateMeans(means: number[]) {
+//   let i = 0;
+//   let accumulate = means.reduce((acc, current) => acc + current, i); 
+//   return accumulate;
+// }
+
+
+
+const meanDA = Object.values(dataIntervalMeanDA.m_pa_fp);
+// let accumulateMeanDA : number[] = [];
+// accumulateMeanDA.push(accumulateMeans(meanDA));
+
+// console.log(accumulateMeanDA.push(accumulateMeans(meanDA)));
+
+
+const meanDR = Object.values(dataIntervalMeanDR.m_pr_fp);
+const meanConsumiption = Object.values(dataIntervalMeanConsumiption.m_c_fp);
 
 export const data = {
   labels,
@@ -127,31 +129,35 @@ export const data = {
       label: "Demanda Ativa",
       borderColor: "rgb(24, 255, 0)",
       backgroundColor: "rgb(24, 255, 0)",
-      data: Object.values(dataIntervalMeanDA.m_pa_p),
+      data: meanDA,
       yAxisID: "y",
+      pointStyle: "rectRot",
+      hoverRadius: 6
     },
     {
       label: "Demanda Reativa",
-      data: Object.values(dataIntervalMeanDR),
+      data: meanDR,
       borderColor: "rgb(255, 0, 0)",
       backgroundColor: "rgb(255, 0, 0)",
       yAxisID: "y",
+      pointStyle: "rectRot",
+      hoverRadius: 6
     },
     {
       label: "Consumo em Reais (R$)",
-      data: Object.values(dataIntervalMeanConsumiption),
+      data: meanConsumiption,
       borderColor: "rgb(15, 38, 241)",
       backgroundColor: "rgb(15, 38, 241)",
       yAxisID: "y1",
+      pointStyle: "rectRot",
+      hoverRadius: 6
     },
   ],
 };
 
-
-
 export function ChartDiary() {
-
   const [showModal, setShowModal] = useState(false);
+  const [datasetIndex, setDatasetIndex] = useState(0);
 
   function closeModal() {
     setShowModal(false);
@@ -160,13 +166,16 @@ export function ChartDiary() {
   const openModal = (element: InteractionItem[]) => {
     if (!element.length) return;
 
-    console.log(element.length);
+    // console.log(element.length);
+
     setShowModal(true);
-    // const { datasetIndex, index } = element[0];
 
+    const { datasetIndex, index } = element[0];
 
+    setDatasetIndex(datasetIndex);
+    console.log(datasetIndex);
 
-    // console.log(data.labels[index], data.datasets[datasetIndex].data[index]);
+    // console.log(data.labels[index], data.datasets[datasetIndex].label);
   };
 
   const chartRef = useRef<ChartJS>(null);
@@ -180,8 +189,6 @@ export function ChartDiary() {
     openModal(getElementAtEvent(chart, event));
   };
 
-
-  
   return (
     <div>
       <Chart
@@ -191,7 +198,28 @@ export function ChartDiary() {
         options={options}
         data={data}
       />
-      {showModal ? <Modal closeModal={closeModal} /> : ""}
+      {showModal ? (
+        <Modal
+          measurements={
+            datasetIndex === 0
+              ? meanDA
+              : datasetIndex === 1
+              ? meanDR
+              : datasetIndex === 2
+              ? meanConsumiption
+              : []
+          }
+          title={data.datasets[datasetIndex].label}
+          period="14 de outubro de 2022"
+          closeModal={closeModal}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
+
+// 0 = Demanda Ativa
+// 1 = Demanda Reativa
+// 2 = Consumo em Reais
