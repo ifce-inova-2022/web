f68da849df721597ddaa9043b4394aae8e2adb72
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-12-21 13:21:48 -0300

    chore: config host

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3030 },
+  // server: { port: 3030 },
 });
