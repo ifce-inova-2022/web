7fe4c03079946a20b421338b65f068c416b10af3
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2023-01-13 10:43:45 -0300

    style: add border rounded

--- src/components/SectionDashboard.tsx
+++ src/components/SectionDashboard.tsx
@@ -7,7 +7,7 @@ interface SectionDashboardProps {
 
 export function SectionDashboard({ title, children }: SectionDashboardProps) {
   return (
-    <section className="w-full h-full flex flex-col sm:gap-6 gap-2 text-zinc-700 bg-zinc-100 sm:pb-0 pb-20 rounded-tl-2xl sm:p-10 p-6 sm:px-20 px-4 ">
+    <section className="flex-1 w-full min-h-screen flex flex-col sm:gap-6 gap-2 text-zinc-700 bg-zinc-100 sm:pb-0 pb-20 rounded-tl-2xl sm:rounded-tr-none rounded-tr-2xl sm:p-10 p-6 md:px-20 sm:px-10 px-4 ">
       <h1 className="sm:text-3xl text-2xl font-bold">{title}</h1>
       {children}
     </section>
